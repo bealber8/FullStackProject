2a20181d606b429562e09dc98f3cf0c969802f7e
bealber8 <beacalle1997@gmail.com>
2018-12-17 12:24:51 +0000

    Async method

--- Spring/toyota/src/main/java/com/beatriz/toyota/controllers/FutureModelsController.java
+++ Spring/toyota/src/main/java/com/beatriz/toyota/controllers/FutureModelsController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -70,6 +71,7 @@ public class FutureModelsController {
 		
 	}
 	
+	@Async
 	void writeImageFile(byte[] bytes) {
 		byte[] data = Base64.getDecoder().decode(bytes);
 		try(OutputStream stream = new FileOutputStream("otrapruebita.jpg")){
